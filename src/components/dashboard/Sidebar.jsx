0509bd8ef498238b514ecdbc1fe7936ad8dbef0e
samuel-akinboro <victorakinboro2003@gmail.com>
2024-08-18 17:46:12 +0100

    logout fix

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { auth } from "@/firebaseConfig";
+import { signOut } from "firebase/auth";
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
